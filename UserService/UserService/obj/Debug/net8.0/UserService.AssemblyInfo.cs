2b717255ae15c103ca36ac1fc74c4bd7d6024e27
Riya Jain <riya.jain15@outlook.com>
2025-03-12 20:48:37 +0530

    #added vs code

--- UserService/UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
+++ UserService/UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f65457e890542b8c85344e0cf0577c58a73413d")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
