92d155ef1dfeb89914b43a0f955374a2d19da4a5
Riya Jain <riya.jain15@outlook.com>
2025-03-14 14:53:27 +0530

    #changes for userauthentication

---
+++ UserAuthentication/ThriftStore.UserAuthentication.IAM/Data/Entities/Base.cs
@@ -0,0 +1,15 @@
+﻿
+using ThriftStore.UserAuthentication.IAM.Enums;
+
+namespace ThriftStore.UserAuthentication.IAM.Data.Entities
+{
+    public class Base
+    {
+        public DateTime? CreatedAt { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string? UpdatedBy { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
